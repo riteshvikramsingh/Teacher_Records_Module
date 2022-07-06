f73c5586360eb970a22284cdbbe5cab23822ac8b
riteshvikramsingh <91671050+riteshvikramsingh@users.noreply.github.com>
2022-07-06 15:45:23 +0530

    Add files via upload

---
+++ Teacher_Records/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Teacher_Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            TeacherData.Teachernfo();
+        }       
+    }
+}
---
+++ Teacher_Records/TeacherData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Teacher_Records
+{
+    class TeacherData
+    {
+        public static void Teachernfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Teacher Information!!!");
+            string path = Directory.GetCurrentDirectory();
+            string filename = "TeacherRecords.txt";
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File is not present, creating new file");
+            }
+            else
+            {
+                Console.WriteLine("File is Present!!!");
+            }
+            using (StreamWriter writer = File.CreateText(filename))
+            {
+                writer.WriteLine("ID: T01; Name: Sanjeev; Class:10; Section:A");
+                writer.WriteLine("ID: T04; Name: Meetu; Class:09; Section:A");
+                writer.WriteLine("ID: T06; Name: Senthil; Class:10; Section:B");
+                writer.WriteLine("ID: T02; Name: Chinmay; Class:08; Section:A");
+                writer.WriteLine("ID: T03; Name: Vamsi; Class:09; Section:B");
+                writer.WriteLine("ID: T05; Name: Viswes; Class:08; Section:B");
+            }
+
+            string text = File.ReadAllText(filename);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Teacher Infomation Displayed");
+
+        }
+    }
+}
+
